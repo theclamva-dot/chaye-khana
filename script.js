8db7cb214d8dbe00d849c0a791154266bf79b031
theclamva-dot <theclamva@gmail.com>
2026-08-09 21:41:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.getElementById("menu-btn");
+const darkModeBtn = document.getElementById("dark-mode");
+const gallery = document.querySelector(".gallery");
+
+console.log("JavaScript Connected Successfully!");
+
+alert("Welcome to Chaykhana ☕");
